eeea7cbf6a6a0e624e22fad8b67b12924937ff86
Trevor <tms245@cornell.edu>
2012-10-27 00:39:38 -0400

    compiling hashtable implementation based on Foster's notes

--- shared/hashtable.ml
+++ shared/hashtable.ml
@@ -1,19 +1,71 @@
-type ('a, 'b) t
+type ('a, 'b) t = 
+  { items : int ref;
+    table : ((('a * 'b) list) array) ref;
+    hash : 'a -> int }
 
 let create capacity hash = 
-  failwith "Die monster. You don't belong in this world!"
+  { items = ref 0; 
+    table = ref (Array.make capacity []);
+    hash = hash }
+    
+let rec resize h =
+  let n = Array.length !(h.table) in
+  let h' = create (2 * n) h.hash in
+  for i = 0 to n - 1 do
+    List.iter
+      (fun (k,v) -> add h' k v) 
+      !(h.table).(i)
+  done;
+  h.table := !(h'.table)
+    
+and add table key value = 
+  if !(table.items) >= Array.length !(table.table) * 2 then 
+    resize table;
+  let i = table.hash key in 
+  !(table.table).(i) <- (key, value)::!(table.table).(i);
+  table.items := !(table.items) + 1
 
-let add table key value = 
-  failwith "It was not by my hand that I am once again given flesh. I was called here by humans, who wish to pay me tribute."
+(* needs to raise Not_found exception appropriately *)  
+let find table key = 
+  let i = table.hash key in
+  snd (List.hd !(table.table).(i))
+(*  let i = table.hash key in
+  try Some (List.assoc key !(table.table).(i)) 
+  with Not_found -> None
+*)
 
-let find table key = failwith "Tribute? You steal men's souls, and make them your slaves."
+let mem table key = 
+  let i = table.hash key in
+  if !(table.table).(i) = [] then false else true
 
-let mem table key = failwith "Perhaps the same could be said of all religions."
+let remove table key = 
+  let i = table.hash key in 
+  let rec pop acc l = 
+    match l with 
+    | [] -> (false,List.rev acc)
+    | (k',v)::t -> if k' = key then (true,List.rev_append acc t) else pop ((k',v)::acc) t in 
+  let b,l' = pop [] !(table.table).(i) in 
+  !(table.table).(i) <- l';
+  if b then table.items := !(table.items) - 1 else ()
 
-let remove table key = failwith "Your words are as empty as your soul. Mankind ill needs a savior such as you."
+let iter (f : 'a -> 'b -> unit) table =
+  let stop = (Array.length !(table.table)) - 1 in
+  let f' (a, b) =
+    f a b
+  in
+  for i = 0 to stop do
+    List.hd (List.map f' !(table.table).(i));
+  done
+      
+let fold f table init = 
+  let stop = (Array.length !(table.table)) - 1 in
+  let f' acc (a, b) =
+    f a b acc
+  in
+  let acc = ref init in
+  for i = 0 to stop do
+    acc := List.fold_left f' !acc !(table.table).(i);
+  done;
+  !acc
 
-let iter f table = failwith "What is a man?! A miserable little pile of secrets! But enough talk, have at you!"
-
-let fold f table init = failwith "(The battle rages)"
-
-let length table = failwith "No! This cannot be! AAAAAAAAAH!!!"
+let length table = table.items
