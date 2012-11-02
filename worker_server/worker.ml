d1bd0e7df280b9dd1c42c133ba00e5030bf1385a
joe <joe@joe-ubuntu-laptop.(none)>
2012-11-01 20:31:05 -0400

    Adding locks to activeWorker

--- worker_server/worker.ml
+++ worker_server/worker.ml
@@ -3,6 +3,8 @@ open Util
 
 let activeWorkers = Hashtbl.create 10 
 
+let m = Mutex.create ()
+
 let send_response client response =
   let success = Connection.output client response in
     (if not success then
@@ -14,15 +16,19 @@ let send_response client response =
 let mapper_builder source shared_data : worker_response = 
   match Program.build source with
   | (Some(id), _) -> 
-    (Hashtbl.add activeWorkers id "mapper"; 
-      Program.write_shared_data id shared_data;
-      Mapper(Some(id),shared_data))
+    (Mutex.lock m;
+    Hashtbl.add activeWorkers id "mapper"; 
+    Program.write_shared_data id shared_data;
+    Mutex.unlock m;
+    Mapper(Some(id),shared_data))
   | (None, error_msg) -> Mapper(None, error_msg) 
 
 let reducer_builder source : worker_response =
   match Program.build source with 
   | (Some(id), _) -> 
-    (Hashtbl.add activeWorkers id "reducer"; 
+    (Mutex.lock m;
+      Hashtbl.add activeWorkers id "reducer"; 
+      Mutex.unlock m;
       Reducer(Some(id),""))
   | (None, error_msg) -> Reducer(None, error_msg)
 
@@ -48,7 +54,12 @@ let rec handle_request client =
         if (send_response client built) then handle_request client else ()
     | MapRequest (id, k, v) -> 
         let response = 
-        if (Hashtbl.mem activeWorkers id && Hashtbl.find activeWorkers id = "mapper") then
+        let isMap = 
+          Mutex.lock m;
+          (Hashtbl.mem activeWorkers id 
+          && Hashtbl.find activeWorkers id = "mapper") in
+          Mutex.unlock m;
+        if isMap then
           let results = Program.run id (k,v)
           in map_requester client id results
         else 
@@ -56,7 +67,12 @@ let rec handle_request client =
         if response then handle_request client else ()
     | ReduceRequest (id, k, v) -> 
         let response = 
-        if (Hashtbl.mem activeWorkers id && Hashtbl.find activeWorkers id = "reducer") then
+        let isReduce = 
+          Mutex.lock m;
+          (Hashtbl.mem activeWorkers id 
+          && Hashtbl.find activeWorkers id = "reducer") in
+          Mutex.unlock m;
+        if isReduce then
           let results = Program.run id (k,v)
           in red_requester client id results
         else 
