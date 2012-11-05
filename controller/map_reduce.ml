362e4c7bf3020cb2ee31d62a624f0fdd9feaf657
joe <joe@joe-ubuntu-laptop.(none)>
2012-11-04 22:57:48 -0500

    New map_reduce. Still hangs on inverted_index/reuters

--- controller/map_reduce.ml
+++ controller/map_reduce.ml
@@ -1,7 +1,100 @@
 open Util
 open Worker_manager
-
+(*
 let map kv_pairs shared_data map_filename : (string * string) list = 
+  let worker_manager =
+    ref (Worker_manager.initialize_mappers map_filename shared_data)
+  in
+  let final_output = ref [] in
+  let work_status = ref (Hashtbl.create 16) in
+  List.iter (fun (k,v) -> Hashtbl.add !work_status (k,v) false) kv_pairs;
+  let remaining_work = ref kv_pairs in
+  let pool = ref (Thread_pool.create 20) in
+  let m = Mutex.create () in
+  let assign (k,v) () =
+    let worker = Worker_manager.pop_worker !worker_manager in
+    match (Worker_manager.map worker k v) with
+    | None -> ()
+    | Some lst ->
+        Worker_manager.push_worker !worker_manager worker;
+        Mutex.lock m;
+        final_output := (List.rev_append lst !final_output);
+        Hashtbl.replace !work_status (k,v) true;
+        Mutex.unlock m in
+  while !remaining_work <> [] do
+    List.iter 
+      (fun (k,v) -> 
+        Thread.delay 0.1; 
+        Thread_pool.add_work (assign (k,v)) !pool) !remaining_work;
+    remaining_work := Hashtbl.fold 
+      (fun kv is_done acc -> 
+        if not is_done then kv::acc else acc) !work_status []
+  done;
+  Worker_manager.clean_up_workers !worker_manager;
+  Thread_pool.destroy !pool;
+  !final_output
+  *)
+
+type worker_types = Map | Reduce
+
+(*
+    match map_or_reduce with
+    | Map -> ref (Worker_manager.initialize_mappers filename shared_data)
+    | Reduce -> ref (Worker_manager.initialize_reducers filename shared_data) in
+*)
+
+(*
+      match map_or_reduce with
+      | Map -> Worker_manager.map worker k v
+      | Reduce -> Worker_manager.reduce worker k v in
+*)
+
+(*
+          (match map_or_reduce with 
+          | Map -> List.rev_append lst !final_output
+          | Reduce -> (k,lst)::(!final_output));
+*)
+
+let helper pairs worker_manager work output =
+  let final_output = ref [] in
+  let work_status = ref (Hashtbl.create 16) in
+  List.iter (fun (k,v) -> Hashtbl.add !work_status (k,v) false) pairs;
+  let remaining_work = ref pairs in
+  let pool = ref (Thread_pool.create 20) in
+  let m = Mutex.create () in
+  let assign (k,v) () =
+    let worker = Worker_manager.pop_worker !worker_manager in
+    let results = work worker k v in
+    match results with
+    | None -> ()
+    | Some lst ->
+        Worker_manager.push_worker !worker_manager worker;
+        Mutex.lock m;
+        output k lst final_output;
+        Hashtbl.replace !work_status (k,v) true;
+        Mutex.unlock m in
+  while !remaining_work <> [] do
+    List.iter 
+      (fun (k,v) -> 
+        Thread.delay 0.1; 
+        Thread_pool.add_work (assign (k,v)) !pool) !remaining_work;
+    remaining_work := Hashtbl.fold 
+      (fun kv is_done acc -> 
+        if not is_done then kv::acc else acc) !work_status []
+  done;
+  Thread_pool.destroy !pool;
+  Worker_manager.clean_up_workers !worker_manager;
+  !final_output
+
+let map kv_pairs shared_data map_filename : (string * string) list =
+  let worker_manager = 
+    ref (Worker_manager.initialize_mappers map_filename shared_data) in
+  let work =
+    Worker_manager.map in
+  let output k lst final_output = 
+    final_output := List.rev_append lst !final_output in
+  helper kv_pairs worker_manager work output
+(*
   let worker_manager =
     ref (Worker_manager.initialize_mappers map_filename shared_data)
   in
@@ -28,7 +121,8 @@ let map kv_pairs shared_data map_filename : (string * string) list =
   Worker_manager.clean_up_workers !worker_manager;
   Thread_pool.destroy !pool;
   !final_output
-  
+*)
+
 let combine kv_pairs : (string * string list) list = 
   let tbl = Hashtbl.create (List.length kv_pairs) in 
   let output = ref [] in
@@ -44,6 +138,14 @@ let combine kv_pairs : (string * string list) list =
   Hashtbl.iter make_list tbl; !output   
 
 let reduce kvs_pairs shared_data reduce_filename : (string * string list) list =
+  let worker_manager = 
+    ref (Worker_manager.initialize_reducers reduce_filename "") in
+  let work =
+    Worker_manager.reduce in
+  let output k lst final_output = 
+    final_output := (k,lst)::(!final_output) in
+  helper kvs_pairs worker_manager work output
+(*
   let worker_manager = 
     ref (Worker_manager.initialize_reducers reduce_filename "")
   in
@@ -70,7 +172,7 @@ let reduce kvs_pairs shared_data reduce_filename : (string * string list) list =
   Worker_manager.clean_up_workers !worker_manager;
   Thread_pool.destroy !pool;
   !final_output
-
+*)
 let map_reduce (app_name : string) (mapper : string) 
     (reducer : string) (filename : string) =
   let app_dir = Printf.sprintf "apps/%s/" app_name in
