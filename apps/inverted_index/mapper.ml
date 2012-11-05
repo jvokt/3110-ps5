663fc68803e5d1d281a86e1cea53846442a9e1d9
Trevor <tms245@cornell.edu>
2012-11-05 18:28:41 -0500

    made deduplication step the reducer's work

--- apps/inverted_index/mapper.ml
+++ apps/inverted_index/mapper.ml
@@ -1,8 +1,3 @@
 let (file, contents) = Program.get_input() in
-let used = Hashtbl.create 16 in
-let just_once acc word = 
-  if Hashtbl.mem used word then acc
-  else (Hashtbl.add used word "present"; (word, file)::acc)
-in
-Program.set_output (List.fold_left just_once []
+Program.set_output (List.fold_left (fun acc word -> (word, file)::acc) []
                    (List.map String.lowercase (Util.split_words contents)))
--- apps/inverted_index/reducer.ml
+++ apps/inverted_index/reducer.ml
@@ -1,2 +1,7 @@
 let (word, docs) = Program.get_input() in
-  Program.set_output (List.sort compare docs)
+let used = Hashtbl.create 16 in
+let just_once acc doc = 
+  if Hashtbl.mem used doc then acc
+  else (Hashtbl.add used doc "present"; doc::acc) in 
+let docs = List.fold_left just_once [] docs in
+Program.set_output (List.sort compare docs)
