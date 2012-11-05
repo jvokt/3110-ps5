757192e8155a8f6889df1b982eb8da1512465a26
Trevor <tms245@cornell.edu>
2012-11-04 19:48:50 -0500

    one slight adjustment

--- apps/nbody/mapper.ml
+++ apps/nbody/mapper.ml
@@ -2,9 +2,7 @@ let (id1, id2) = Program.get_input() in
 let shared = Util.unmarshal (Program.get_shared_data()) in
 let body1 = Hashtbl.find shared id1 in
 let body2 = Hashtbl.find shared id2 in
-let acceleration body1 body2 =
-  let (_, l1, _) = body1 in
-  let (m2, l2, _) = body2 in 
+let acceleration (_, l1, _) (m2, l2, _) =
   let mag = (Util.cBIG_G *. m2) /. ((Plane.s_dist l1 l2)**2.0) in
   let dir = Plane.unit_vector l1 l2 in   
   Plane.scale_point mag dir in
