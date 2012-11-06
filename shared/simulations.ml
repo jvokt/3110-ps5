db515ecf42fb874491745fbdc1511cd2e08cd0fb
joe <joe@joe-ubuntu-laptop.(none)>
2012-11-06 00:16:59 -0500

    Trinary Star and Deuterium Atom

--- shared/simulations.ml
+++ shared/simulations.ml
@@ -167,4 +167,42 @@ let terrible_situation =
   ("3", bodies 2 [])
 
 let zardoz = 
-  ("Farewell, land of my birth. Never again will these eyes gaze upon your beauty.", [])
+  let pos = function
+    | 0 -> (50.,0.)
+    | 1 -> (-25.0,44.3)
+    | _ -> (-25.0,-44.3) in
+  let vel = function
+    | 0 -> (0.,3.) 
+    | 1 -> ((3./.2.)*.(-1.77),(3./.2.)*.(-1.0))
+    | _ -> ((3./.2.)*.1.77,(3./.2.)*.(-1.0)) in
+  let mass = mass *. 150. in
+  let rec bodies n acc = 
+    if n < 0 then acc
+    else
+      bodies (n-1) ((string_of_int n, (mass, pos n, vel n))::acc) in
+  ("3",bodies 2 [])
+
+(* Behold the deuterium ion *)
+let deuterium_ion =
+  let proton_mass = 1.6749*.mass in
+  let neutron_mass = 1.6726*.mass in
+  let nucleus_mass = proton_mass +. neutron_mass in
+  let electron_mass = 0.91094*.mass in
+  let electron_velocity = 1.19 in
+  let mass = function
+    | 0 -> nucleus_mass
+    | 1 -> electron_mass
+    | _ -> electron_mass in
+  let pos = function
+    | 0 -> origin
+    | 1 -> (0.,50.)
+    | _ -> (0., (-50.)) in 
+  let vel = function
+    | 0 -> origin
+    | 1 -> (electron_velocity,0.)
+    | _ -> ((-.electron_velocity), (0.)) in 
+  let rec bodies n acc = 
+    if n < 0 then acc
+    else
+      bodies (n-1) ((string_of_int n, (mass n, pos n, vel n))::acc) in
+  ("3",bodies 2 [])
