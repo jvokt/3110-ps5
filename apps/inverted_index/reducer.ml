dee08bb515c7465719247da244b9a993ae25b0ba
Trevor <tms245@cornell.edu>
2012-11-08 22:14:13 -0500

    reverted deduplication step back to mapper's work

--- apps/inverted_index/reducer.ml
+++ apps/inverted_index/reducer.ml
@@ -1,9 +1,4 @@
 let (word, docs) = Program.get_input() in
-let used = Hashtbl.create 16 in
-let just_once acc doc = 
-  if Hashtbl.mem used doc then acc
-  else (Hashtbl.add used doc "present"; doc::acc) in 
-let docs = List.fold_left just_once [] docs in
 let c a b = 
   let a' = String.length a in
   let b' = String.length b in
